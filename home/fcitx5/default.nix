199241b6d7950c035eed6dad3465526d11ee9731
HJ-Zhang <hjzhang216@gmail.com>
2025-05-15 20:42:54 +0800

    update

--- home/fcitx5/default.nix
+++ home/fcitx5/default.nix
@@ -1,11 +1,15 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    fcitx5-chinese-addons
-    fcitx5-configtool
-    fcitx5-pinyin-zhwiki
-  ];
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-chinese-addons
+      fcitx5-configtool
+      fcitx5-pinyin-zhwiki
+    ];
+  };
 
   home.file = {
     ".local/share/fcitx5/themes/macOS-dark" = {
@@ -18,12 +22,11 @@
   };
 
   home.sessionVariables = {
-    GTK_IM_MODULE="fcitx";
-    QT_IM_MODULE="fcitx";
-    XMODIFIERS="@im=fcitx";
-    INPUT_METHOD="fcitx";
-    SDL_IM_MODULE="fcitx";
-    GLFW_IM_MODULE="ibus";
-    HM_TEST="hello world";
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
+    INPUT_METHOD = "fcitx";
+    SDL_IM_MODULE = "fcitx";
+    GLFW_IM_MODULE = "ibus";
   };
 }
--- home/programs/fonts.nix
+++ home/programs/fonts.nix
@@ -8,13 +8,15 @@
     nerd-fonts.hack
   ];
 
-  # 自动链接 Nix 安装的字体
-  home.file.".local/share/fonts".source =
-    let
-      fontsDrv = pkgs.buildEnv {
-        name = "nix-fonts";
-        paths = config.home.packages;
-        pathsToLink = [ "/share/fonts" ];
-      };
-    in "${fontsDrv}/share/fonts";
+  home.file.".local/share/fonts/nix" = {
+    source =
+      let
+        fontsDrv = pkgs.buildEnv {
+          name = "nix-fonts";
+          paths = config.home.packages;
+          pathsToLink = [ "/share/fonts" ];
+        };
+      in "${fontsDrv}/share/fonts";
+    recursive = true;
+  };
 }
